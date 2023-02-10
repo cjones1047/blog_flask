0094d65ed1f1f903840bd9968d0f92e25c73db46
cjones1047 <cjmj1047@gmail.com>
2023-02-10 08:05:16 -0600

    Added href attribute to each blog post on index.html that links to the full post; each post href calls get_post() in main.py to retrieve post with 'id' attribute that matches post_id parameter; get_post() renders post.html after finding requested blog post

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template
 import requests
 
-
 app = Flask(__name__)
+
+blog_posts_response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+blog_posts_response.raise_for_status()
+blog_posts = blog_posts_response.json()
 
 
 @app.route('/')
 def home():
-    blog_posts_response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
-    blog_posts_response.raise_for_status()
-    blog_posts = blog_posts_response.json()
+    global blog_posts
 
     return render_template("index.html", blog_posts=blog_posts)
+
+
+@app.route("/post/<int:post_id>")
+def get_post(post_id):
+    global blog_posts
+    post = None
+    for this_post in blog_posts:
+        if this_post["id"] == post_id:
+            post = this_post
+            break
+
+    return render_template("post.html", post=post)
 
 
 if __name__ == "__main__":
