4df514bba806e331e57b86592b944c471dccb1fa
cjones1047 <cjmj1047@gmail.com>
2023-02-10 07:35:27 -0600

    home() in main.py queries API for list of blog posts; index.html iterates over list of blog posts and renders each's title and subtitle

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_posts_response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    blog_posts_response.raise_for_status()
+    blog_posts = blog_posts_response.json()
+
+    return render_template("index.html", blog_posts=blog_posts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
